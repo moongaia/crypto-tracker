3161fdefe71af99fd7d21aa84ca3ca1568c71cdf
abigailjy90 <70828921+abigailjy90@users.noreply.github.com>
2021-12-07 13:08:07 +0900

    typing the themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+function App() {
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Protected</H1>
+    </Container>
   );
 }
-
 export default App;
