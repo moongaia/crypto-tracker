29d248bd999b545c1781c0c04bf35671320304f5
abigailjy90 <70828921+abigailjy90@users.noreply.github.com>
2021-12-07 12:11:27 +0900

    optional props

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,18 +1,30 @@
 import styled from "styled-components";
 
-const Container = styled.div<CircleProps>`
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string;
+}
+
+const Container = styled.div<ContainerProps>`
   width: 200px;
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `; //styled-components가 props받음.
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string; //optional props
+  text?: string;
 } // interface는 object를 설명해주는 것. object의 shape(형태)을 설명.
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
-}
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
+} // borderColor가 undefined이면 기본값을 보냄.
 
 export default Circle;
