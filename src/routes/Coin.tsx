33bff866bb7163136e29ef4072ac3581d4e62217
abigailjy90 <70828921+abigailjy90@users.noreply.github.com>
2021-12-09 20:10:29 +0900

    route states

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,11 +1,50 @@
-import { useParams } from "react-router";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router";
+import styled from "styled-components";
 
-interface Params {
+interface RouteParams {
   coinId: string;
 }
 
+interface RouteState {
+  name: string;
+}
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
 function Coin() {
-  const { coinId } = useParams<Params>();
-  return <h1>Coin: {coinId}</h1>;
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams<RouteParams>();
+  const { state } = useLocation<RouteState>();
+
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading..."} </Title>
+        {/* state가 존재하면 name을 가져오고, 존재하지 않으면 loading */}
+      </Header>
+      {loading ? <Loader>Loading..</Loader> : null}
+    </Container>
+  );
 }
 export default Coin;
